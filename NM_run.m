6e9cd9fb529fbe35e7e914ef16ec45647ba49578
Ryan Garry <regarry@ncsu.edu>
2025-01-17 13:43:11 -0500

    Rene combine numorph with ryan mods

---
+++ NM_run.m
@@ -0,0 +1,8 @@
+
+% perform channel alignment for a single dataset
+
+sample = 'SAMPLE2';
+[img_directory, output_directory, group] = NM_samples(sample, 'True');
+[config, path_table] = NM_config('process', sample); % config dataset
+[config, path_table] = perform_channel_alignment(config, path_table, 'True'); % channel alignment
+translation_table = reformat(config); % reformat output for Python loading
